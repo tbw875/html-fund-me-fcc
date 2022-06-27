eee5522126ce217d35f26bc0cc637da6585a4358
Tom <charger875@gmail.com>
2022-06-27 15:49:08 -0700

    Lesson 8 13:30:46, Input Forms is next

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+console.log(ethers);
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         window.ethereum.request({ method: "eth_requestAccounts" });
-        document.getElementById("connectButton").innerHTML = "Connected";
+        connectButton.innerHTML = "Connected";
         console.log("Metamask connected!");
     } else {
-        document.getElementById("connectButton").innerHTML =
-            "Please install Metamask";
+        connectButton.innerHTML = "Please install Metamask";
     }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = "0.1";
     console.log(`Funding with ${ethAmount}`);
+    if (typeof window.ethereum !== "undefined") {
+        // Need: Provider / connection; signer ; contract (ABI + Address)
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            });
+            await listenForTransactionMine(transactionResponse, provider);
+            console.log("Done!");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}`);
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            );
+            resolve();
+        });
+    });
 }
